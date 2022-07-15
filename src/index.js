353d2ed8ee2d90682af7769ad1ecb89971f2fe2a
Laaguini <vovanlihoi@gmail.com>
2022-07-15 14:55:48 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Motion from './motion.js'
+import Fr45 from './fr45.js';
+import Fr46 from './fr46.js'
+import Fr47 from './fr47.js';
+import Fr48 from './fr48.js'
+import Fr49 from './fr49.js';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Motion>
+      <Fr45 />
+      <Fr46 />
+      <Fr47 />
+      <Fr48 />
+      <Fr49 />
+    </Motion>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
