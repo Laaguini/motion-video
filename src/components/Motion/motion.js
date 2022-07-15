77754bbdf30bd29483fbe4bda82b74e8bbebc39f
Laaguini <vovanlihoi@gmail.com>
2022-07-15 15:51:57 +0300

    moved Motion to its folder

--- src/components/Motion/motion.js
+++ src/components/Motion/motion.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from "react"
-import "./motion.css"
+import style from "./motion.module.css"
 
 const Motion = ({children}) => {
     const [current, setCurrent] = useState(0)
@@ -15,8 +15,8 @@ const Motion = ({children}) => {
     return (
         <div className="motion-video">
             {children.length > 1 &&
-            children.map((Child, i) =>  (i === current) && <div key={i} className={`motion-wrapper`}>{Child}</div>)
-            || <div className={`motion-wrapper`}>{children}</div>
+            children.map((Child, i) =>  (i === current) && <div key={i} className={style[`motion-wrapper`]}>{Child}</div>)
+            || <div className={style[`motion-wrapper`]}>{children}</div>
         }
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Motion from './motion.js'
+import Motion from './components/Motion/motion.js'
 import Fr45 from './components/Rules/Rules.js';
 import Fr46 from './components/Costs/Costs.js'
 import Fr47 from './components/AffiliateProgram/AffiliateProgram.js';
