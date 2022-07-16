3ca717df5e886a1328f49215bd8fcdc7db877977
Laaguini <vovanlihoi@gmail.com>
2022-07-16 15:13:04 +0300

    Update Rules.js

--- src/pages/Rules.js
+++ src/pages/Rules.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react"
+
 import Motion from '../components/Motion/motion.js'
 import Rules from '../components/Rules/Rules.js';
 import Costs from '../components/Costs/Costs.js'
@@ -6,12 +8,22 @@ import BudgetAllocation from '../components/BudgetAllocation/BudgetAllocation.js
 import LevelStructure from '../components/LevelStructure/LevelStructure.js';
 import '../index.css'
 
-const RulesPage = () => <Motion>
-    <Rules />
-    <Costs />
-    <AffiliateProgram />
-    <BudgetAllocation />
-    <LevelStructure />
-</Motion>
+const RulesPage = () => {
+    useEffect(() => {
+        setTimeout(() => {
+            window.location.assign("/")
+        }, 25000)
+    })
+
+    return (
+        <Motion>
+            <Rules />
+            <Costs />
+            <AffiliateProgram />
+            <BudgetAllocation />
+            <LevelStructure />
+        </Motion>
+    )
+}
 
 export default RulesPage
